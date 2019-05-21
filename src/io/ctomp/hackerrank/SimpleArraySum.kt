4542b9c5e0965972c0a897d8e9a6179cf567d410
Carl Tompkins <carl@ctomp.io>
2019-05-20 20:46:58 -0400

    Implement SimpleArraySum

---
+++ src/io/ctomp/hackerrank/SimpleArraySum.kt
@@ -0,0 +1,31 @@
+package io.ctomp.hackerrank
+
+import java.io.*
+import java.math.*
+import java.text.*
+import java.util.*
+import java.util.regex.*
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+fun simpleArraySum(ar: Array<Int>): Int {
+	/*
+	 * Write your code here.
+	 */
+	return ar.reduce {
+		sum, num -> sum + num
+	}
+}
+
+fun main(args: Array<String>) {
+	val scan = Scanner(System.`in`)
+
+	val arCount = scan.nextLine().trim().toInt()
+
+	val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+	val result = simpleArraySum(ar)
+
+	println(result)
+}
